f0c4369119f31837232462a9629d52be206ad473
hanjo8813 <hanjo8813@gmail.com>
2022-03-05 03:23:18 +0900

    chore: init project

---
+++ src/main/java/com/example/socketserver/SocketServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.socketserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocketServerApplication.class, args);
+    }
+
+}
