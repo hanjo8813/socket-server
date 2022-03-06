d63eaf743e502f95f73fbca8e133c9cbfec0274e
hanjo8813 <hanjo8813@gmail.com>
2022-03-06 22:25:47 +0900

    fix: 스케줄러 cron 오류 수정

--- src/main/java/com/example/socketserver/common/scheduler/HealthScheduler.java
+++ src/main/java/com/example/socketserver/common/scheduler/HealthScheduler.java
@@ -15,8 +15,8 @@ public class HealthScheduler {
     private static final String URL = "https://kkakdduk.herokuapp.com/health";
     private final RestTemplate restTemplate = new RestTemplate();
 
-    // 매일 pm3:00 ~ am2:00 20분마다 실행됨
-    @Scheduled(cron = "0 0/20 15-2 * * *", zone = "Asia/Seoul")
+    // 매일 pm2:00 ~ am2:00 20분마다 실행됨
+    @Scheduled(cron = "0 0/20 0-2,14-23 * * *", zone = "Asia/Seoul")
     public void healthCheck() {
         ResponseEntity<String> response = restTemplate.getForEntity(URL, String.class);
         log.info("{}", response.getBody());
