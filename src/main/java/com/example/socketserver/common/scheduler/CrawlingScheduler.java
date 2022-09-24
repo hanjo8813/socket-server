aa86749fdc35f55a863f7032cbfb50d5f91e1f59
한재원 <hanjo8813@gmail.com>
2022-09-24 15:06:55 +0900

    tmp

--- src/main/java/com/example/socketserver/common/scheduler/CrawlingScheduler.java
+++ src/main/java/com/example/socketserver/common/scheduler/CrawlingScheduler.java
@@ -29,40 +29,41 @@ public class CrawlingScheduler {
     @Value("${SLACK_URL}")
     private String BOT;
 
-    private static final String NAVER_URL = "http://api.booking.naver.com/v3.0/businesses/562202/biz-items/4030151/daily-schedules?lang=ko&endDateTime=2022-11-05T00:00:00&isRestaurant=false&startDateTime=2022-09-25T00:00:00";
+//    private static final String NAVER_URL = "http://api.booking.naver.com/v3.0/businesses/562202/biz-items/4030151/daily-schedules?lang=ko&endDateTime=2022-11-05T00:00:00&isRestaurant=false&startDateTime=2022-09-25T00:00:00";
+//
+//    @Scheduled(cron = "0 0/1 * * * *", zone = "Asia/Seoul")
+//    public void naver() throws JsonProcessingException {
+//        log.info("산너머 스케줄러 실행");
+//
+//        ResponseEntity<String> response = restTemplate.getForEntity(NAVER_URL, String.class);
+//        ObjectMapper mapper = new ObjectMapper();
+//        JsonNode root = mapper.readTree(response.getBody());
+//
+//        JsonNode target1 = root.path("2022-10-08");
+//        int stock1 = target1.path("stock").asInt();
+//        int bookingCount1 = target1.path("bookingCount").asInt();
+//        log.info("예약 정보 확인 / stock1 : {} / bookingCount1 : {}", stock1, bookingCount1);
+//
+//        JsonNode target2 = root.path("2022-10-09");
+//        int stock2 = target2.path("stock").asInt();
+//        int bookingCount2 = target2.path("bookingCount").asInt();
+//        log.info("예약 정보 확인 / stock2 : {} / bookingCount2 : {}", stock2, bookingCount2);
+//
+//        Map<String,Object> request = new HashMap<>();
+//        if(stock1 > bookingCount1){
+//            request.put("text", "산너머(토요일) 예약뜸~!~ https://m.booking.naver.com/booking/3/bizes/562202/items/4030151 여기서 빨리 예약 ㄱㄱ");
+//            HttpEntity<Map<String,Object>> entity = new HttpEntity<>(request);
+//            restTemplate.exchange(BOT, HttpMethod.POST, entity, String.class);
+//        }
+//        if(stock2 > bookingCount2){
+//            request.put("text", "산너머(일요일) 예약뜸~!~ https://m.booking.naver.com/booking/3/bizes/562202/items/4030151 여기서 빨리 예약 ㄱㄱ");
+//            HttpEntity<Map<String,Object>> entity = new HttpEntity<>(request);
+//            restTemplate.exchange(BOT, HttpMethod.POST, entity, String.class);
+//        }
+//    }
+
     private static final String CAMP_URL_B = "https://baekdo.ticketplay.zone/portal/realtime/productSelect?room_area_no=11&stay_cnt=1&check_in=20221008&check_out=20221009";
     private static final String CAMP_URL_C = "https://baekdo.ticketplay.zone/portal/realtime/productSelect?room_area_no=12&stay_cnt=1&check_in=20221008&check_out=20221009";
-
-    @Scheduled(cron = "0 0/1 * * * *", zone = "Asia/Seoul")
-    public void naver() throws JsonProcessingException {
-        log.info("산너머 스케줄러 실행");
-
-        ResponseEntity<String> response = restTemplate.getForEntity(NAVER_URL, String.class);
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode root = mapper.readTree(response.getBody());
-
-        JsonNode target1 = root.path("2022-10-08");
-        int stock1 = target1.path("stock").asInt();
-        int bookingCount1 = target1.path("bookingCount").asInt();
-        log.info("예약 정보 확인 / stock1 : {} / bookingCount1 : {}", stock1, bookingCount1);
-
-        JsonNode target2 = root.path("2022-10-09");
-        int stock2 = target2.path("stock").asInt();
-        int bookingCount2 = target2.path("bookingCount").asInt();
-        log.info("예약 정보 확인 / stock2 : {} / bookingCount2 : {}", stock2, bookingCount2);
-
-        Map<String,Object> request = new HashMap<>();
-        if(stock1 > bookingCount1){
-            request.put("text", "산너머(토요일) 예약뜸~!~ https://m.booking.naver.com/booking/3/bizes/562202/items/4030151 여기서 빨리 예약 ㄱㄱ");
-            HttpEntity<Map<String,Object>> entity = new HttpEntity<>(request);
-            restTemplate.exchange(BOT, HttpMethod.POST, entity, String.class);
-        }
-        if(stock2 > bookingCount2){
-            request.put("text", "산너머(일요일) 예약뜸~!~ https://m.booking.naver.com/booking/3/bizes/562202/items/4030151 여기서 빨리 예약 ㄱㄱ");
-            HttpEntity<Map<String,Object>> entity = new HttpEntity<>(request);
-            restTemplate.exchange(BOT, HttpMethod.POST, entity, String.class);
-        }
-    }
 
     @Scheduled(cron = "0 0/1 * * * *", zone = "Asia/Seoul")
     public void camp() throws IOException {
